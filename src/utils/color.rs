2856d70c4f67d421dbc22db82aff5c827c4b6482
saikuru0 <me@saikuru.net>
2025-04-24 00:29:43 +0200

    alpha 0 oopsie

--- src/utils/color.rs
+++ src/utils/color.rs
@@ -5,7 +5,7 @@ pub fn kanii_to_rgba(color: Color) -> Option<[u8; 4]> {
         let r: u8 = rgb.0 as u8;
         let g: u8 = rgb.1 as u8;
         let b: u8 = rgb.2 as u8;
-        Some([r, g, b, 0])
+        Some([r, g, b, 255])
     } else {
         None
     }
