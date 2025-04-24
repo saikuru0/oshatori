8517dd48a13ab785e7fb7700a3a691449ce635d1
saikuru0 <me@saikuru.net>
2025-04-24 14:58:27 +0200

    color sign and alpha fix

--- src/utils/color.rs
+++ src/utils/color.rs
@@ -1,11 +1,8 @@
 use kanii_lib::packets::types::Color;
 
 pub fn kanii_to_rgba(color: Color) -> Option<[u8; 4]> {
-    if let Ok(rgb) = color.as_rgb() {
-        let r: u8 = rgb.0 as u8;
-        let g: u8 = rgb.1 as u8;
-        let b: u8 = rgb.2 as u8;
-        Some([r, g, b, 255])
+    if let Ok(rgba) = color.as_rgba() {
+        Some(rgba)
     } else {
         None
     }
