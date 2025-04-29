536f56b5e6b67b653fe6b8ca468207b4244e5060
saikuru0 <me@saikuru.net>
2025-04-29 16:48:35 +0200

    oopsie newline

--- src/utils/html.rs
+++ src/utils/html.rs
@@ -4,10 +4,9 @@ pub fn parse_html(s: String) -> String {
     let re = Regex::new(r"&lt;|&gt;|\s<br/>\s").unwrap();
 
     re.replace_all(&s, |caps: &regex::Captures| match &caps[0] {
-        r"&lt;" => "<",
-        r"&gt;" => ">",
-        r"\s<br/>\s" => "\n",
-        _ => "",
+        "&lt;" => "<",
+        "&gt;" => ">",
+        _ => "\n",
     })
     .to_string()
 }
